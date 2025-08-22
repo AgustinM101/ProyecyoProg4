93e9850d6d0c055ba2382976e6eb5028eabea186
Ana Gonzalez <anavel15@live.com.ar>
2025-08-21 23:30:28 -0300

    creacion de rutas nuevas

---
+++ prog4back/app/Router/Routes/PlanAlimentoRoutes.php
@@ -0,0 +1,68 @@
+<?php 
+
+final readonly class PlanAlimentoRoutes {
+  public static function getRoutes(): array {
+    return [
+
+
+     [
+        "name" => "PlanAlimento_create",
+        "url" => "/planAlimentos",
+        "controller" => "PlanAlimento/PlanAlimentoPostController.php",
+        "method" => "POST"
+      ],
+     
+      [
+        "name" => "planAlimento_get",
+        "url" => "/planAlimentos",
+        "controller" => "PlanAlimento/PlanAlimentoGetController.php",
+        "method" => "GET",
+        "parameters" => [
+          [
+            "name" => "id",
+            "type" => "int",
+          ]
+        ]
+      ],
+      [
+        "name" => "planAlimentos_get",
+        "url" => "/planAlimentos",
+        "controller" => "PlanAlimento/PlanAlimentosGetController.php",
+        "method" => "GET"
+      ],
+      [
+        "name" => "planAlimento_create",
+        "url" => "/planAlimentos",
+        "controller" => "PlanAlimento/PlanAlimentoPostController.php",
+        "method" => "POST"
+      ],
+      [
+        "name" => "planAlimento_update",
+        "url" => "/planAlimentos",
+        "controller" => "PlanAlimento/PlanAlimentoPutController.php",
+        "method" => "PUT",
+        "parameters" => [
+          [
+            "name" => "id",
+            "type" => "int",
+
+          ]
+        ]
+          ],
+          [
+            "name" => "planAlimento_delete",
+            "url" => "/planAlimentos",
+            "controller" => "PlanAlimento/PlanAlimentoDeleteController.php",
+            "method" => "DELETE",
+            "parameters" => [
+              [
+                "name" => "id",
+                "type" => "int"
+              ]
+            ]
+          ]
+
+          
+    ];
+  }
+}
