6c2c0d334a7d6f4d0f087910b039b6f8c017f587
AgustinM101 <agustinmasso@hotmail.com>
2025-08-26 18:54:21 -0300

    actualizado front

--- prog4front/src/App.jsx
+++ prog4front/src/App.jsx
@@ -1,20 +1,22 @@
-import './App.css'
+import './App.css';
 import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
-import {  LoginRegister } from './components/LoginRegisterForms/LoginRegister';
 
+import { MantineProvider } from '@mantine/core';
+import { RegisterPage } from './pages/RegisterPage/RegisterPage';
+import { LoginPage } from './pages/LoginPage/LoginPage';
+import { BrowserRouter, Routes, Route } from "react-router";
+import { HomePage } from './pages/HomePage/HomePage';
 
 function App() {
- 
-
-
   return (
-    <MantineProvider>
-        
-
-        <LoginRegister/>
-       
-
+    <MantineProvider defaultColorScheme="dark">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </MantineProvider>
   )
 }
