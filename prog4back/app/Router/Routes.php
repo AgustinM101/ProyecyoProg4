06e24838d534d1e0f6767743c45d6fc56417a163
AgustinM101 <agustinmasso@hotmail.com>
2025-10-07 16:48:15 -0300

    eliminados archivos innecesarios

--- prog4back/app/Router/Routes.php
+++ prog4back/app/Router/Routes.php
@@ -31,8 +31,7 @@ function startRouter(): Router
     include_once "Routes/FileRoutes.php";
     $routes = array_merge($routes, FileRoutes::getRoutes());
 
-    include_once "Routes/ClientRoutes.php";
-    $routes = array_merge($routes, ClientRoutes::getRoutes());
+
 
     $routesClass = [];
     foreach ($routes as $route) {
