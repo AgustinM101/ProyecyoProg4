1bd53a3dbd4e954a62e3f73a626371704bc1cccc
AgustinM101 <agustinmasso@hotmail.com>
2025-10-18 22:53:56 -0300

    cambios en purchase y payment service

--- prog4front/src/services/PaymentService.js
+++ prog4front/src/services/PaymentService.js
@@ -1,15 +1,9 @@
-export async function createPaymentPreference(data) {
-  const response = await fetch("http://localhost/backend/payment", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
+import { api } from "./api";
 
-  if (!response.ok) {
-    throw new Error("Error al crear la preferencia de pago");
-  }
 
-  return response.json();
+export const PaymentService = {
+
+  createPayment: (data) => api.post("/payment", data)
+
+
 }
